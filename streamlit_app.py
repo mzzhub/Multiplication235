87ba9ac7de4ae39adecabc1a4224a2b55720b7ca
mzhub <mishabhadhi@gmail.com>
2025-02-17 20:56:30 +0530

    len

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ message = False
 if st.button("**Geneate solutions**"):
     digits = [0,1,2,3,4,5,6,7,8,9]
     permutations = list(itertools.permutations(digits))
+    st.write(len(permutations))
 
     sl_no = 1
 
@@ -37,4 +38,3 @@ if message:
                             Total of <span style='color:red;'>16</span> solutions found.
                         </h1>
                     """, unsafe_allow_html=True)
-                    
