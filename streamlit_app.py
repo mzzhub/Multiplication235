ab6bed0b1c278870adb270140d9761d9d04f5d26
mzhub <mishabhadhi@gmail.com>
2025-02-17 20:12:57 +0530

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,16 @@ st.title('Problem - Multiplication235')
 
 st.image("problem.png")
 
+# col1, col2, col3 = st.columns([1,1,1])
+# with col2:
+#     generate_button = st.button("**Geneate solutions**")
+
+# if generate_button:
+#     st.write("Ai")
+
 col1, col2, col3 = st.columns([1,1,1])
 with col2:
-    generate_button = st.button("**Geneate solutions**")
-
-if generate_button:
-    st.write("Ai")
+    if st.button("**Geneate solutions**"):
+        st.write("hai")
 
 
