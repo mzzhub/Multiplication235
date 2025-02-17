ff6ac87654245bf4f8d9acc6807430d5666af696
mzhub <mishabhadhi@gmail.com>
2025-02-17 20:11:47 +0530

    if

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,14 @@
 import streamlit as st
 
-st.title('Problem - Multiplicarion235')
+st.title('Problem - Multiplication235')
 
 st.image("problem.png")
+
+col1, col2, col3 = st.columns([1,1,1])
+with col2:
+    generate_button = st.button("**Geneate solutions**")
+
+if generate_button:
+    st.write("Ai")
 
 
